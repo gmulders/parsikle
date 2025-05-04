06c7d0fff62daf423f501b8dff680d0b8d6fa478
Geert Mulders <gmulders@gmail.com>
2025-05-04 08:00:53 +0200

    Cleanup commented code

--- parsikle/src/commonMain/kotlin/io/github/gmulders/parsikle/core/TokenParser.kt
+++ parsikle/src/commonMain/kotlin/io/github/gmulders/parsikle/core/TokenParser.kt
@@ -2,41 +2,11 @@ package io.github.gmulders.parsikle.core
 
 data class TokenMatch<T>(val type: T, val match: MatchResult) {
     val matchValue: String get() = match.value
-
-//    override fun hashCode(): Int {
-//        var result = type?.hashCode() ?: 0
-//        result = 31 * result + matchValue.hashCode()
-//        return result
-//    }
-//
-//    override fun equals(other: Any?): Boolean {
-//        if (this === other) return true
-//        if (other == null || other !is TokenMatch<T>) return false
-//        if (type != other.type) return false
-//        if (matchValue != other.matchValue) return false
-//        return true
-//    }
 }
 
 data class RegularExpressionError(val regex: Regex, val haystack: CharSequence) : Error {
     override val message: String
         get() = "Could not match regex '$regex' in '$haystack'"
-
-//    override fun hashCode(): Int {
-//        var result = regex.toString().hashCode()
-//        result = 31 * result + regex.options.hashCode()
-//        result = 31 * result + haystack.hashCode()
-//        return result
-//    }
-//
-//    override fun equals(other: Any?): Boolean {
-//        if (this === other) return true
-//        if (other == null || other !is RegularExpressionError) return false
-//        if (regex.toString() != other.regex.toString()) return false
-//        if (regex.options != other.regex.options) return false
-//        if (haystack != other.haystack) return false
-//        return true
-//    }
 }
 
 fun <T> tokenParser(re: Regex, type: T): Parsikle<TokenMatch<T>> = { state ->
